92ec0597325d5c87e9d94e4d69ea826f4384bbe7
Phcode007 <paulobelarmino047@gmail.com>
2026-03-03 09:13:11 -0300

    feat: add authentication context and dynamic layout for professional registration

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    staticGenerationRetryCount: 1,
+  },
+  staticPageGenerationTimeout: 240,
+  onDemandEntries: {
+    maxInactiveAge: 25 * 1000,
+    pagesBufferLength: 5,
+  },
 };
 
 export default nextConfig;
