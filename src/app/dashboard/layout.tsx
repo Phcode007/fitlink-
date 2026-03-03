94530533a6539adaef0860aa1037a4f35354b554
Phcode007 <paulobelarmino047@gmail.com>
2026-03-03 09:24:37 -0300

    feat: add dynamic layout and revalidation for dashboard

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,10 @@
+export const dynamic = 'force-dynamic'
+export const revalidate = false
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return children
+}
